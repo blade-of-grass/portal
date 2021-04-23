245e967b95410642e12005bf9961e189b67523db
Mark Grass <mgrass79@gmail.com>
2021-04-23 09:15:14 -0400

    moved some constants to config.h

---
+++ configs.h
@@ -0,0 +1,8 @@
+#ifndef __configs_h__
+#define __configs_h__
+
+#define WINDOW_WIDTH 1024
+#define WINDOW_HEIGHT 1024
+#define WINDOW_TITLE "portal"
+
+#endif // __configs_h__
--- main.c
+++ main.c
@@ -1,29 +1,31 @@
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
 
+#include "configs.h"
 #include "shape.h"
 #include <stdio.h>
 
 int main(void)
 {
-    struct Shape shape;
-
+    // create window
     glfwInit();
-
     glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
-    GLFWwindow* window = glfwCreateWindow(800, 600, "Vulkan window", NULL, NULL);
+    glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+    GLFWwindow* window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_TITLE, NULL, NULL);
 
+    // log vulkan extension count
     uint32_t extensionCount = 0;
     vkEnumerateInstanceExtensionProperties(NULL, &extensionCount, NULL);
 
     printf("%d extensions supported", extensionCount);
 
+    // program loop
     while (!glfwWindowShouldClose(window)) {
         glfwPollEvents();
     }
 
+    // cleanup
     glfwDestroyWindow(window);
-
     glfwTerminate();
 
     return 0;
